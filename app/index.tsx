4d247f56d8ed0e0e6995ef9e8ba2777e8d3d9156
Rudra <rudralocking@gmail.com>
2026-07-06 00:04:03 +0530

    feat: implement camera permission handling and update UI for camera view

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,28 @@
-import { Text, View } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+
+  const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+
+  if (!cameraPermission) {
+    return <View />
+  }
+
+  if (!cameraPermission.granted) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-center text-xl mb-4">
+          We need your permission to start the game.
+        </Text>
+        <Button onPress={requestCameraPermission} title="Grant Permission" accessibilityLabel="Grant Permission" />
+      </View>
+    )
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <View className="flex-1 justify-center items-center">
+      <CameraView className="flex-1" facing="back" style={{ width: "100%", height: "100%" }} />
     </View>
   );
 }
